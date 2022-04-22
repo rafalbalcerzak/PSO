2ec68fc8fb9d141361565565892b16755748b3d0
rafalbalcerzak <raffi.balcer@gmail.com>
2022-04-22 21:57:08 +0200

    osobno liczenie i pokaz

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import imageio
-import os
 
 
 class Swarm:
     def __init__(self, epoch: int, n_particles: int, lb: list, ub: list, c: list, w: float, fun: callable):
         self._epoch = epoch
         self._n_particles = n_particles
-        self._lb = lb
-        self._ub = ub
-        self._c = c
-        self._w = w
-        self._fun = fun
         self._swarm_best = None
         self._swarm = []
         for _ in range(self._n_particles):
-            self._swarm.append(Particle(self._lb, self._ub, self._c, self._w, self._fun, self))
+            self._swarm.append(Particle(lb, ub, c, w, fun, self))
+
+    def get_swarm(self):
+        return [particle._position for particle in self._swarm]
 
     @property
     def swarm_best(self):
@@ -29,9 +24,15 @@ class Swarm:
     def optimize(self):
         for e in range(self._epoch):
             for particle in self._swarm:
-                particle.update()
-            print(self._swarm_best)
-        return self._swarm_best
+                if e < self._epoch/3:
+                    particle.update(explore=True)
+                else:
+                    particle.update()
+        return True
+
+    def step(self, explore=False):
+        for particle in self._swarm:
+            particle.update(explore=explore)
 
 
 class Particle:
@@ -47,7 +48,7 @@ class Particle:
         self._velocity = np.random.uniform(np.array(self._lb) / 2, np.array(self._ub) / 2, len(self._lb))
         self._position = np.random.uniform(self._lb, self._ub, len(self._lb))
 
-        self.cost = cost_fun(self._position)
+        self.cost = fun(self._position)
         self._particle_best = [self._position, self.cost]
 
         if self._parent.swarm_best is None:
@@ -90,67 +91,24 @@ class Particle:
             self._parent.swarm_best = [self._position, self._cost]
 
 
-def cost_fun(x):
-    return (1.5 - x[0] - x[0] * x[1]) ** 2 +\
-           (2.25 - x[0] + (x[0] * x[1]) ** 2) ** 2 +\
-           (2.625 - x[0] + (x[0] * x[1]) ** 3) ** 2
 
+if __name__ == '__main__':
+    def cost_fun(x):
+        return (1.5 - x[0] - x[0] * x[1]) ** 2 +\
+               (2.25 - x[0] + (x[0] * x[1]) ** 2) ** 2 +\
+               (2.625 - x[0] + (x[0] * x[1]) ** 3) ** 2
 
-# swarm (epoch: int, n_particles: int, lb: list, ub: list, c: list, w: float, fun: callable)
-swarm = Swarm(40, 20, [-4.5, -4.5], [4.5, 4.5], [0.4, 0.2], np.random.uniform(0.1, 0.8), cost_fun)
-swarm.optimize()
+    # swarm (epoch: int, n_particles: int, lb: list, ub: list, c: list, w: float, fun: callable)
+    swarm = Swarm(40, 20, [-4.5, -4.5], [4.5, 4.5], [0.4, 0.2], np.random.uniform(0.4, 0.9), cost_fun)
+    if swarm.optimize():
+        print(swarm.swarm_best)
 
-'''swarm_best = None
+    def cost3(x):
+        return (1.5 - x[0] - x[2] * x[1]) ** 2 +\
+               (2.25 - x[0] + (x[2] * x[1]) ** 2) ** 2 +\
+               (2.625 - x[0] + (x[2] * x[1]) ** 3) ** 2
 
-swarm = []
-for _ in range(20):
-    swarm.append(particle([-4.5, -4.5], [4.5, 4.5], [0.4, 0.2], np.random.uniform(0.1, 0.8)))
-
-epoch = 100
-bests = []
-filenames = []
-
-for e in range(epoch):
-    fig.tight_layout()
-    fig, axs = plt.subplots(2, 1, figsize=(5, 10))
-    x = []
-    [x.append(pos.position()[0]) for pos in swarm]
-    y = []
-    [y.append(pos.position()[1]) for pos in swarm]
-    w = []
-    [w.append(pos._w) for pos in swarm]
-    axs[0].scatter(x, y, alpha=0.5)
-    axs[0].set_xlim([-5, 5])
-    axs[0].set_ylim([-5, 5])
-    bests.append(swarm_best[1])
-    bests_plt = np.array(bests)
-    axs[1].set_title("Function minimum")
-    axs[1].set_ylabel('Value')
-    axs[1].set_xlabel('Epoch')
-    axs[1].set_yscale('log')
-    axs[1].plot(np.linspace(0, len(bests_plt), len(bests_plt)), bests_plt)
-
-    # create file
-    filename = f'{e}.png'
-    filenames.append(filename)
-
-    # save frame
-    plt.savefig(filename)
-    plt.close()
-
-    for particle in swarm:
-        if e < epoch / 3:
-            particle.update(explore=True)
-        else:
-            particle.update()
-
-print(swarm_best)
-
-with imageio.get_writer('pso.gif', mode='I') as writer:
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
-
-# Remove files
-for filename in set(filenames):
-    os.remove(filename)'''
+    # swarm (epoch: int, n_particles: int, lb: list, ub: list, c: list, w: float, fun: callable)
+    swarm = Swarm(40, 20, [-4.5, -4.5, -4.5], [4.5, 4.5, 4.5], [0.4, 0.2], np.random.uniform(0.4, 0.9), cost3)
+    if swarm.optimize():
+        print(swarm.swarm_best)
---
+++ visual.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+import os
+
+from main import Swarm, Particle
+
+def cost(x):
+    return (1.5 - x[0] - x[0] * x[1]) ** 2 +\
+           (2.25 - x[0] + (x[0] * x[1]) ** 2) ** 2 +\
+           (2.625 - x[0] + (x[0] * x[1]) ** 3) ** 2
+
+
+# swarm (epoch: int, n_particles: int, lb: list, ub: list, c: list, w: float, fun: callable)
+swarm = Swarm(40, 20, [-4.5, -4.5], [4.5, 4.5], [0.4, 0.2], np.random.uniform(0.1, 0.8), cost)
+epoch = 50
+filenames = []
+bests = []
+for e in range(epoch):
+    fig, axs = plt.subplots(2, 1, figsize=(5, 10))
+    fig.tight_layout()
+    x = np.array(swarm.get_swarm()[0:])[:, 0]
+    y = np.array(swarm.get_swarm()[0:])[:, 1]
+
+    axs[0].scatter(x, y, alpha=0.5)
+    axs[0].set_xlim([-5, 5])
+    axs[0].set_ylim([-5, 5])
+
+    bests.append(swarm.swarm_best[1])
+    bests_plt = np.array(bests)
+    axs[1].set_title("Function minimum")
+    axs[1].set_ylabel('Value')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_yscale('log')
+    axs[1].plot(np.linspace(0, len(bests_plt), len(bests_plt)), bests_plt)
+
+    # create file
+    filename = f'{e}.png'
+    filenames.append(filename)
+
+    # save frame
+    plt.savefig(filename)
+    plt.close()
+
+    if e < 20:
+        swarm.step(True)
+    else:
+        swarm.step()
+print(swarm.swarm_best)
+with imageio.get_writer('pso.gif', mode='I') as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+
+# Remove files
+for filename in set(filenames):
+    os.remove(filename)
